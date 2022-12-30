2b9848f0e9acdd463796b635353c41d9899c8bf9
changaolee <changao.li.life@outlook.com>
2022-12-31 02:05:06 +0800

    feat: 添加日志功能（阶段1、不可编译）

---
+++ internal/pkg/log/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2022 lichangao(李长傲) <changao.li.work@outlook.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file. The original repo for
+// this file is https://github.com/changaolee/skeleton.
+
+package log // import "github.com/changaolee/skeleton/internal/pkg/log"
