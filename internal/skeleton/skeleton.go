20598ff72922b9c1ba1783b3eaadc898c714513c
lichangao <changao.li.work@outlook.com>
2022-12-31 02:51:25 +0800

    feat: 添加日志功能（阶段3：日志包初始化和调用）

--- internal/skeleton/skeleton.go
+++ internal/skeleton/skeleton.go
@@ -6,67 +6,73 @@
 package skeleton
 
 import (
-	"encoding/json"
-	"fmt"
+    "encoding/json"
+    "fmt"
 
-	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
+    "github.com/spf13/cobra"
+    "github.com/spf13/viper"
+
+    "github.com/changaolee/skeleton/internal/pkg/log"
 )
 
 var cfgFile string
 
 // NewSkeletonCommand 创建一个 *cobra.Command 对象。之后，可以使用 Command 对象的 Execute 方法来启动应用程序。
 func NewSkeletonCommand() *cobra.Command {
-	cmd := &cobra.Command{
-		// 指定命令的名字，该名字会出现在帮助信息中
-		Use: "skeleton",
-		// 命令的简短描述
-		Short: "A good Go skeleton project",
-		// 命令的详细描述
-		Long: `A good Go practical project.
+    cmd := &cobra.Command{
+        // 指定命令的名字，该名字会出现在帮助信息中
+        Use: "skeleton",
+        // 命令的简短描述
+        Short: "A good Go skeleton project",
+        // 命令的详细描述
+        Long: `A good Go practical project.
 
 Find more miniblog information at:
         https://github.com/changaolee/skeleton#readme`,
 
-		// 命令出错时，不打印帮助信息。不需要打印帮助信息，设置为 true 可以保持命令出错时一眼就能看到错误信息
-		SilenceUsage: true,
-		// 指定调用 cmd.Execute() 时，执行的 Run 函数，函数执行失败会返回错误信息
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return run()
-		},
-		// 这里设置命令运行时，不需要指定命令行参数
-		Args: func(cmd *cobra.Command, args []string) error {
-			for _, arg := range args {
-				if len(arg) > 0 {
-					return fmt.Errorf("%q does not take any arguments, got %q", cmd.CommandPath(), args)
-				}
-			}
-			return nil
-		},
-	}
+        // 命令出错时，不打印帮助信息。不需要打印帮助信息，设置为 true 可以保持命令出错时一眼就能看到错误信息
+        SilenceUsage: true,
+        // 指定调用 cmd.Execute() 时，执行的 Run 函数，函数执行失败会返回错误信息
+        RunE: func(cmd *cobra.Command, args []string) error {
+            // 初始化日志
+            log.Init(logOptions())
+            defer log.Sync() // Sync 将缓存中的日志刷新到磁盘文件中
 
-	// 以下设置，使得 initConfig 函数在每个命令运行时都会被调用以读取配置
-	cobra.OnInitialize(initConfig)
+            return run()
+        },
+        // 这里设置命令运行时，不需要指定命令行参数
+        Args: func(cmd *cobra.Command, args []string) error {
+            for _, arg := range args {
+                if len(arg) > 0 {
+                    return fmt.Errorf("%q does not take any arguments, got %q", cmd.CommandPath(), args)
+                }
+            }
+            return nil
+        },
+    }
 
-	// 在这里您将定义标志和配置设置
+    // 以下设置，使得 initConfig 函数在每个命令运行时都会被调用以读取配置
+    cobra.OnInitialize(initConfig)
 
-	// Cobra 支持持久性标志（PersistentFlag），该标志可用于它所分配的命令以及该命令下的每个子命令
-	cmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "The path to the skeleton configuration file. Empty string for no configuration file.")
+    // 在这里您将定义标志和配置设置
 
-	// Cobra 也支持本地标志，本地标志只能在其所绑定的命令上使用
-	cmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+    // Cobra 支持持久性标志（PersistentFlag），该标志可用于它所分配的命令以及该命令下的每个子命令
+    cmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "The path to the skeleton configuration file. Empty string for no configuration file.")
 
-	return cmd
+    // Cobra 也支持本地标志，本地标志只能在其所绑定的命令上使用
+    cmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+    return cmd
 }
 
 // run 实际的业务代码入口函数。
 func run() error {
-	// 打印所有的配置项及其值
-	settings, _ := json.Marshal(viper.AllSettings())
-	fmt.Println(string(settings))
+    // 打印所有的配置项及其值
+    settings, _ := json.Marshal(viper.AllSettings())
+    log.Infow(string(settings))
 
-	// 打印 db -> username 配置项的值
-	fmt.Println(viper.GetString("db.username"))
+    // 打印 db -> username 配置项的值
+    log.Infow(viper.GetString("db.username"))
 
-	return nil
+    return nil
 }
