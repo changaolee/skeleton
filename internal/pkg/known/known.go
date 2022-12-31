51a1e3363be1c8be2ece41c33e4f5cd24ba92c69
lichangao <changao.li.work@outlook.com>
2022-12-31 16:45:57 +0800

    feat: add license

--- internal/pkg/known/known.go
+++ internal/pkg/known/known.go
@@ -1,6 +1,11 @@
+// Copyright 2022 lichangao(李长傲) <changao.li.work@outlook.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file. The original repo for
+// this file is https://github.com/changaolee/skeleton.
+
 package known
 
 const (
-    // XRequestIDKey 用来定义 Gin 上下文中的键，代表请求的 uuid
-    XRequestIDKey = "X-Request-ID"
+	// XRequestIDKey 用来定义 Gin 上下文中的键，代表请求的 uuid
+	XRequestIDKey = "X-Request-ID"
 )
