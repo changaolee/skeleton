b01e0db58b77b60c062a4ed990b9ccd3e4477369
lichangao <changao.li.work@outlook.com>
2022-12-31 23:14:17 +0800

    feat: add license

--- internal/pkg/core/core.go
+++ internal/pkg/core/core.go
@@ -1,3 +1,8 @@
+// Copyright 2022 lichangao(李长傲) <changao.li.work@outlook.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file. The original repo for
+// this file is https://github.com/changaolee/skeleton.
+
 package core
 
 import (
