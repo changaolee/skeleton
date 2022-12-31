07669911c183ff78dc8f056ee32eaf030aa630c8
lichangao <changao.li.work@outlook.com>
2022-12-31 14:11:15 +0800

    feat: add license

--- internal/skeleton/skeleton.go
+++ internal/skeleton/skeleton.go
@@ -6,100 +6,100 @@
 package skeleton
 
 import (
-    "errors"
-    "fmt"
-    "net/http"
+	"errors"
+	"fmt"
+	"net/http"
 
-    "github.com/gin-gonic/gin"
-    "github.com/spf13/cobra"
-    "github.com/spf13/viper"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 
-    "github.com/changaolee/skeleton/internal/pkg/log"
-    "github.com/changaolee/skeleton/pkg/version/verflag"
+	"github.com/changaolee/skeleton/internal/pkg/log"
+	"github.com/changaolee/skeleton/pkg/version/verflag"
 )
 
 var cfgFile string
 
 // NewSkeletonCommand 创建一个 *cobra.Command 对象。之后，可以使用 Command 对象的 Execute 方法来启动应用程序。
 func NewSkeletonCommand() *cobra.Command {
-    cmd := &cobra.Command{
-        // 指定命令的名字，该名字会出现在帮助信息中
-        Use: "skeleton",
-        // 命令的简短描述
-        Short: "A good Go skeleton project",
-        // 命令的详细描述
-        Long: `A good Go practical project.
+	cmd := &cobra.Command{
+		// 指定命令的名字，该名字会出现在帮助信息中
+		Use: "skeleton",
+		// 命令的简短描述
+		Short: "A good Go skeleton project",
+		// 命令的详细描述
+		Long: `A good Go practical project.
 
 Find more miniblog information at:
         https://github.com/changaolee/skeleton#readme`,
 
-        // 命令出错时，不打印帮助信息。不需要打印帮助信息，设置为 true 可以保持命令出错时一眼就能看到错误信息
-        SilenceUsage: true,
-        // 指定调用 cmd.Execute() 时，执行的 Run 函数，函数执行失败会返回错误信息
-        RunE: func(cmd *cobra.Command, args []string) error {
-            // 如果 `--version=true`，则打印版本并退出
-            verflag.PrintAndExitIfRequested()
+		// 命令出错时，不打印帮助信息。不需要打印帮助信息，设置为 true 可以保持命令出错时一眼就能看到错误信息
+		SilenceUsage: true,
+		// 指定调用 cmd.Execute() 时，执行的 Run 函数，函数执行失败会返回错误信息
+		RunE: func(cmd *cobra.Command, args []string) error {
+			// 如果 `--version=true`，则打印版本并退出
+			verflag.PrintAndExitIfRequested()
 
-            // 初始化日志
-            log.Init(logOptions())
-            defer log.Sync() // Sync 将缓存中的日志刷新到磁盘文件中
+			// 初始化日志
+			log.Init(logOptions())
+			defer log.Sync() // Sync 将缓存中的日志刷新到磁盘文件中
 
-            return run()
-        },
-        // 这里设置命令运行时，不需要指定命令行参数
-        Args: func(cmd *cobra.Command, args []string) error {
-            for _, arg := range args {
-                if len(arg) > 0 {
-                    return fmt.Errorf("%q does not take any arguments, got %q", cmd.CommandPath(), args)
-                }
-            }
-            return nil
-        },
-    }
+			return run()
+		},
+		// 这里设置命令运行时，不需要指定命令行参数
+		Args: func(cmd *cobra.Command, args []string) error {
+			for _, arg := range args {
+				if len(arg) > 0 {
+					return fmt.Errorf("%q does not take any arguments, got %q", cmd.CommandPath(), args)
+				}
+			}
+			return nil
+		},
+	}
 
-    // 以下设置，使得 initConfig 函数在每个命令运行时都会被调用以读取配置
-    cobra.OnInitialize(initConfig)
+	// 以下设置，使得 initConfig 函数在每个命令运行时都会被调用以读取配置
+	cobra.OnInitialize(initConfig)
 
-    // 在这里您将定义标志和配置设置
+	// 在这里您将定义标志和配置设置
 
-    // Cobra 支持持久性标志（PersistentFlag），该标志可用于它所分配的命令以及该命令下的每个子命令
-    cmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "The path to the skeleton configuration file. Empty string for no configuration file.")
+	// Cobra 支持持久性标志（PersistentFlag），该标志可用于它所分配的命令以及该命令下的每个子命令
+	cmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "The path to the skeleton configuration file. Empty string for no configuration file.")
 
-    // Cobra 也支持本地标志，本地标志只能在其所绑定的命令上使用
-    cmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// Cobra 也支持本地标志，本地标志只能在其所绑定的命令上使用
+	cmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 
-    // 添加 --version 标志
-    verflag.AddFlags(cmd.PersistentFlags())
+	// 添加 --version 标志
+	verflag.AddFlags(cmd.PersistentFlags())
 
-    return cmd
+	return cmd
 }
 
 // run 实际的业务代码入口函数。
 func run() error {
-    // 设置 Gin 模式
-    gin.SetMode(viper.GetString("runmode"))
+	// 设置 Gin 模式
+	gin.SetMode(viper.GetString("runmode"))
 
-    // 创建 Gin 引擎
-    g := gin.New()
+	// 创建 Gin 引擎
+	g := gin.New()
 
-    // 注册 404 Handler.
-    g.NoRoute(func(c *gin.Context) {
-        c.JSON(http.StatusOK, gin.H{"code": 10003, "message": "Page not found."})
-    })
+	// 注册 404 Handler.
+	g.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"code": 10003, "message": "Page not found."})
+	})
 
-    // 注册 /healthz handler.
-    g.GET("/healthz", func(c *gin.Context) {
-        c.JSON(http.StatusOK, gin.H{"status": "ok"})
-    })
-    // 创建 HTTP Server 实例
-    httpsrv := &http.Server{Addr: viper.GetString("addr"), Handler: g}
+	// 注册 /healthz handler.
+	g.GET("/healthz", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"status": "ok"})
+	})
+	// 创建 HTTP Server 实例
+	httpsrv := &http.Server{Addr: viper.GetString("addr"), Handler: g}
 
-    // 运行 HTTP 服务器
-    // 打印一条日志，用来提示 HTTP 服务已经起来，方便排障
-    log.Infow("Start to listening the incoming requests on http address", "addr", viper.GetString("addr"))
-    if err := httpsrv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-        log.Fatalw(err.Error())
-    }
+	// 运行 HTTP 服务器
+	// 打印一条日志，用来提示 HTTP 服务已经起来，方便排障
+	log.Infow("Start to listening the incoming requests on http address", "addr", viper.GetString("addr"))
+	if err := httpsrv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		log.Fatalw(err.Error())
+	}
 
-    return nil
+	return nil
 }
--- pkg/version/verflag/verflag.go
+++ pkg/version/verflag/verflag.go
@@ -1,98 +1,103 @@
+// Copyright 2022 lichangao(李长傲) <changao.li.work@outlook.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file. The original repo for
+// this file is https://github.com/changaolee/skeleton.
+
 package verflag
 
 import (
-    "fmt"
-    "os"
-    "strconv"
+	"fmt"
+	"os"
+	"strconv"
 
-    "github.com/spf13/pflag"
+	"github.com/spf13/pflag"
 
-    "github.com/changaolee/skeleton/pkg/version"
+	"github.com/changaolee/skeleton/pkg/version"
 )
 
 type versionValue int
 
 // 定义一些常量
 const (
-    VersionFalse versionValue = 0
-    VersionTrue  versionValue = 1
-    VersionRaw   versionValue = 2
+	VersionFalse versionValue = 0
+	VersionTrue  versionValue = 1
+	VersionRaw   versionValue = 2
 )
 
 const (
-    strRawVersion   = "raw"
-    versionFlagName = "version"
+	strRawVersion   = "raw"
+	versionFlagName = "version"
 )
 
 var versionFlag = Version(versionFlagName, VersionFalse, "Print version information and quit.")
 
 func (v *versionValue) IsBoolFlag() bool {
-    return true
+	return true
 }
 
 func (v *versionValue) Get() interface{} {
-    return v
+	return v
 }
 
 // String 实现了 pflag.Value 接口中的 String 方法
 func (v *versionValue) String() string {
-    if *v == VersionRaw {
-        return strRawVersion
-    }
+	if *v == VersionRaw {
+		return strRawVersion
+	}
 
-    return fmt.Sprintf("%v", bool(*v == VersionTrue))
+	return fmt.Sprintf("%v", bool(*v == VersionTrue))
 }
 
 // Set 实现了 pflag.Value 接口中的 Set 方法
 func (v *versionValue) Set(s string) error {
-    if s == strRawVersion {
-        *v = VersionRaw
+	if s == strRawVersion {
+		*v = VersionRaw
 
-        return nil
-    }
-    boolVal, err := strconv.ParseBool(s)
-    if boolVal {
-        *v = VersionTrue
-    } else {
-        *v = VersionFalse
-    }
+		return nil
+	}
+	boolVal, err := strconv.ParseBool(s)
+	if boolVal {
+		*v = VersionTrue
+	} else {
+		*v = VersionFalse
+	}
 
-    return err
+	return err
 }
 
 // Type 实现了 pflag.Value 接口中的 Type 方法
 func (v *versionValue) Type() string {
-    return "version"
+	return "version"
 }
 
 // VersionVar 定义了一个具有指定名称和用法的标志
 func VersionVar(p *versionValue, name string, value versionValue, usage string) {
-    *p = value
-    pflag.Var(p, name, usage)
-    // `--version` 等价于 `--version=true`
-    pflag.Lookup(name).NoOptDefVal = "true"
+	*p = value
+	pflag.Var(p, name, usage)
+	// `--version` 等价于 `--version=true`
+	pflag.Lookup(name).NoOptDefVal = "true"
 }
 
 // Version 包装了 VersionVar 函数
 func Version(name string, value versionValue, usage string) *versionValue {
-    p := new(versionValue)
-    VersionVar(p, name, value, usage)
+	p := new(versionValue)
+	VersionVar(p, name, value, usage)
 
-    return p
+	return p
 }
 
 // AddFlags 在任意 FlagSet 上注册这个包的标志，这样它们指向与全局标志相同的值
 func AddFlags(fs *pflag.FlagSet) {
-    fs.AddFlag(pflag.Lookup(versionFlagName))
+	fs.AddFlag(pflag.Lookup(versionFlagName))
 }
 
 // PrintAndExitIfRequested 将检查是否传递了 `--version` 标志，如果是，则打印版本并退出
 func PrintAndExitIfRequested() {
-    if *versionFlag == VersionRaw {
-        fmt.Printf("%#v\n", version.Get())
-        os.Exit(0)
-    } else if *versionFlag == VersionTrue {
-        fmt.Printf("%s\n", version.Get())
-        os.Exit(0)
-    }
+	if *versionFlag == VersionRaw {
+		fmt.Printf("%#v\n", version.Get())
+		os.Exit(0)
+	} else if *versionFlag == VersionTrue {
+		fmt.Printf("%s\n", version.Get())
+		os.Exit(0)
+	}
 }
