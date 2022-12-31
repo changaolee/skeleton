7c985ca228e1d38993b23326e02482782046898e
changaolee <changao.li.life@outlook.com>
2022-12-31 14:11:15 +0800

    feat: add license

--- internal/skeleton/helper.go
+++ internal/skeleton/helper.go
@@ -6,74 +6,74 @@
 package skeleton
 
 import (
-    "os"
-    "path/filepath"
-    "strings"
+	"os"
+	"path/filepath"
+	"strings"
 
-    "github.com/changaolee/skeleton/internal/pkg/log"
-    "github.com/spf13/cobra"
-    "github.com/spf13/viper"
+	"github.com/changaolee/skeleton/internal/pkg/log"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 const (
-    // recommendedHomeDir 定义放置 skeleton 服务配置的默认目录
-    recommendedHomeDir = ".skeleton"
+	// recommendedHomeDir 定义放置 skeleton 服务配置的默认目录
+	recommendedHomeDir = ".skeleton"
 
-    // defaultConfigName 指定了 skeleton 服务的默认配置文件名
-    defaultConfigName = "skeleton.yaml"
+	// defaultConfigName 指定了 skeleton 服务的默认配置文件名
+	defaultConfigName = "skeleton.yaml"
 )
 
 // initConfig 设置需要读取的配置文件名、环境变量，并读取配置文件内容到 viper 中
 func initConfig() {
-    if cfgFile != "" {
-        // 从命令行选项指定的配置文件中读取
-        viper.SetConfigFile(cfgFile)
-    } else {
-        // 查找用户主目录
-        home, err := os.UserHomeDir()
-        // 如果获取用户主目录失败，打印 `'Error: xxx` 错误，并退出程序（退出码为 1）
-        cobra.CheckErr(err)
+	if cfgFile != "" {
+		// 从命令行选项指定的配置文件中读取
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// 查找用户主目录
+		home, err := os.UserHomeDir()
+		// 如果获取用户主目录失败，打印 `'Error: xxx` 错误，并退出程序（退出码为 1）
+		cobra.CheckErr(err)
 
-        // 将 `$HOME/<recommendedHomeDir>` 目录加入到配置文件的搜索路径中
-        viper.AddConfigPath(filepath.Join(home, recommendedHomeDir))
+		// 将 `$HOME/<recommendedHomeDir>` 目录加入到配置文件的搜索路径中
+		viper.AddConfigPath(filepath.Join(home, recommendedHomeDir))
 
-        // 把当前目录加入到配置文件的搜索路径中
-        viper.AddConfigPath(".")
+		// 把当前目录加入到配置文件的搜索路径中
+		viper.AddConfigPath(".")
 
-        // 设置配置文件格式为 YAML（YAML 格式清晰易读，并且支持复杂的配置结构）
-        viper.SetConfigType("yaml")
+		// 设置配置文件格式为 YAML（YAML 格式清晰易读，并且支持复杂的配置结构）
+		viper.SetConfigType("yaml")
 
-        // 配置文件名称（没有文件扩展名）
-        viper.SetConfigName(defaultConfigName)
-    }
+		// 配置文件名称（没有文件扩展名）
+		viper.SetConfigName(defaultConfigName)
+	}
 
-    // 读取匹配的环境变量
-    viper.AutomaticEnv()
+	// 读取匹配的环境变量
+	viper.AutomaticEnv()
 
-    // 读取环境变量的前缀为 SKELETON，如果是 skeleton，将自动转变为大写
-    viper.SetEnvPrefix("SKELETON")
+	// 读取环境变量的前缀为 SKELETON，如果是 skeleton，将自动转变为大写
+	viper.SetEnvPrefix("SKELETON")
 
-    // 以下 2 行，将 viper.Get(key) key 字符串中 '.' 替换为 '_'
-    replacer := strings.NewReplacer(".", "_")
-    viper.SetEnvKeyReplacer(replacer)
+	// 以下 2 行，将 viper.Get(key) key 字符串中 '.' 替换为 '_'
+	replacer := strings.NewReplacer(".", "_")
+	viper.SetEnvKeyReplacer(replacer)
 
-    // 读取配置文件。如果指定了配置文件名，则使用指定的配置文件，否则在注册的搜索路径中搜索
-    if err := viper.ReadInConfig(); err != nil {
-        log.Errorw("Failed to read viper configuration file", "err", err)
-    }
+	// 读取配置文件。如果指定了配置文件名，则使用指定的配置文件，否则在注册的搜索路径中搜索
+	if err := viper.ReadInConfig(); err != nil {
+		log.Errorw("Failed to read viper configuration file", "err", err)
+	}
 
-    // 打印 viper 当前使用的配置文件，方便 Debug
-    log.Debugw("Using config file", "file", viper.ConfigFileUsed())
+	// 打印 viper 当前使用的配置文件，方便 Debug
+	log.Debugw("Using config file", "file", viper.ConfigFileUsed())
 }
 
 // logOptions 从 viper 中读取日志配置，构建 `*log.Options` 并返回
 // 注意：`viper.Get<Type>()` 中 key 的名字需要使用 `.` 分割，以跟 YAML 中保持相同的缩进
 func logOptions() *log.Options {
-    return &log.Options{
-        DisableCaller:     viper.GetBool("log.disable-caller"),
-        DisableStacktrace: viper.GetBool("log.disable-stacktrace"),
-        Level:             viper.GetString("log.level"),
-        Format:            viper.GetString("log.format"),
-        OutputPaths:       viper.GetStringSlice("log.output-paths"),
-    }
+	return &log.Options{
+		DisableCaller:     viper.GetBool("log.disable-caller"),
+		DisableStacktrace: viper.GetBool("log.disable-stacktrace"),
+		Level:             viper.GetString("log.level"),
+		Format:            viper.GetString("log.format"),
+		OutputPaths:       viper.GetStringSlice("log.output-paths"),
+	}
 }
