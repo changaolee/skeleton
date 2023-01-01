79591fa6c0bc06f160329487eafd1426ac4e0ac6
changaolee <changao.li.life@outlook.com>
2023-01-01 21:38:19 +0800

    feat: 添加身份认证功能

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
 	github.com/gin-gonic/gin v1.8.2
+	github.com/golang-jwt/jwt/v4 v4.4.3
 	github.com/google/uuid v1.1.2
 	github.com/gosuri/uitable v0.0.4
 	github.com/jinzhu/copier v0.3.5
---
+++ internal/pkg/middleware/authn.go
@@ -0,0 +1,31 @@
+// Copyright 2022 lichangao(李长傲) <changao.li.work@outlook.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file. The original repo for
+// this file is https://github.com/changaolee/skeleton.
+
+package middleware
+
+import (
+	"github.com/changaolee/skeleton/internal/pkg/core"
+	"github.com/changaolee/skeleton/internal/pkg/errno"
+	"github.com/changaolee/skeleton/internal/pkg/known"
+	"github.com/changaolee/skeleton/pkg/token"
+	"github.com/gin-gonic/gin"
+)
+
+// Authn 是认证中间件，用来从 gin.Context 中提取 token 并验证 token 是否合法，
+// 如果合法则将 token 中的 sub 作为 <用户名> 存放在 gin.Context 的 XUsernameKey 键中
+func Authn() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// 解析 JWT Token
+		username, err := token.ParseRequest(c)
+		if err != nil {
+			core.WriteResponse(c, errno.ErrTokenInvalid, nil)
+			c.Abort()
+
+			return
+		}
+		c.Set(known.XUsernameKey, username)
+		c.Next()
+	}
+}
