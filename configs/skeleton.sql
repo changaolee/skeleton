4665efe3771464800927067620d28010e36fd6cc
lichangao <changao.li.work@outlook.com>
2023-03-31 17:16:19 +0800

    fix: copyright time

--- configs/skeleton.sql
+++ configs/skeleton.sql
@@ -1,4 +1,4 @@
--- Copyright 2022 lichangao(李长傲) <changao.li.work@outlook.com>. All rights reserved.
+-- Copyright 2023 lichangao(李长傲) <changao.li.work@outlook.com>. All rights reserved.
 -- Use of this source code is governed by a MIT style
 -- license that can be found in the LICENSE file. The original repo for
 -- this file is https://github.com/changaolee/skeleton.
