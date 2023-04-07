d1e6ae41ddd45f2812ea2c518edf7e7e89015700
lichangao <changao.li.work@outlook.com>
2023-04-08 05:51:08 +0800

    feat: remove files

--- pkg/db/doc.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2023 lichangao(李长傲) <changao.li.work@outlook.com>. All rights reserved.
-// Use of this source code is governed by a MIT style
-// license that can be found in the LICENSE file. The original repo for
-// this file is https://github.com/changaolee/skeleton.
-
-package db
-
-// import "github.com/changaolee/skeleton/pkg/db"
