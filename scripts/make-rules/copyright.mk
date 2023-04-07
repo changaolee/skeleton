08ec36e74cbb9c685f4f32fcd278756d8f4bfb5f
lichangao <changao.li.work@outlook.com>
2023-04-08 06:09:06 +0800

    feat: 补充 copyright makefile

---
+++ scripts/make-rules/copyright.mk
@@ -0,0 +1,11 @@
+# ==============================================================================
+# Copyright 相关的 Makefile
+
+.PHONY: copyright.verify
+copyright.verify: tools.verify.addlicense
+	@echo "===========> Verifying the boilerplate headers for all files"
+	@addlicense --check -f $(ROOT_DIR)/scripts/boilerplate.txt $(ROOT_DIR) --skip-dirs=third_party,vendor,_output
+
+.PHONY: copyright.add
+copyright.add: tools.verify.addlicense
+	@addlicense -v -f $(ROOT_DIR)/scripts/boilerplate.txt $(ROOT_DIR) --skip-dirs=third_party,vendor,_output
