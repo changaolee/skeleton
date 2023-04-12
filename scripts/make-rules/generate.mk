44f91f15010d4b4e4cb06b4972caa2bb9ff25808
lichangao <changao.li.work@outlook.com>
2023-04-12 18:11:02 +0800

    feat: make 生成错误码文件

--- scripts/make-rules/generate.mk
+++ scripts/make-rules/generate.mk
@@ -2,12 +2,26 @@
 # 用来进行代码生成的 Makefile
 
 .PHONY: gen.run
-gen.run: gen.clean gen.docgo.doc
+gen.run: gen.clean gen.errcode gen.docgo.doc
 
 .PHONY: gen.clean
 gen.clean:
 	@rm -rf ./api/client/{clientset,informers,listers}
 	@$(FIND) -type f -name '*_generated.go' -delete
+
+.PHONY: gen.errcode
+gen.errcode: gen.errcode.code gen.errcode.doc
+
+.PHONY: gen.errcode.code
+gen.errcode.code: tools.verify.codegen
+	@echo "===========> Generating skt error code go source files"
+	@codegen -type=int ${ROOT_DIR}/internal/pkg/code
+
+.PHONY: gen.errcode.doc
+gen.errcode.doc: tools.verify.codegen
+	@echo "===========> Generating error code markdown documentation"
+	@codegen -type=int -doc \
+		-output ${ROOT_DIR}/docs/guide/zh-CN/api/error_code_generated.md ${ROOT_DIR}/internal/pkg/code
 
 .PHONY: gen.docgo.doc
 gen.docgo.doc:
