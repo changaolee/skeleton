c528a6326c06f269ff68cfe5ef2ea77471e59b3e
lichangao <changao.li.work@outlook.com>
2023-04-13 16:46:28 +0800

    feat: 定义 auto 策略

---
+++ internal/pkg/middleware/auth/auto.go
@@ -0,0 +1,68 @@
+package auth
+
+import (
+	"strings"
+
+	"github.com/changaolee/skeleton/internal/pkg/code"
+	"github.com/changaolee/skeleton/internal/pkg/core"
+	"github.com/changaolee/skeleton/internal/pkg/middleware"
+	"github.com/changaolee/skeleton/pkg/errors"
+	"github.com/gin-gonic/gin"
+)
+
+const authHeaderCount = 2
+
+// AutoStrategy 定义了可自动选择的身份认证策略.
+// 支持在 Basic 和 JWT Bearer 认证间切换.
+type AutoStrategy struct {
+	basic middleware.AuthStrategy
+	jwt   middleware.AuthStrategy
+}
+
+var _ middleware.AuthStrategy = &AutoStrategy{}
+
+// NewAutoStrategy 基于给定的 Basic 和 JWT Bearer 认证策略创建一个 AutoStrategy.
+func NewAutoStrategy(basic, jwt middleware.AuthStrategy) AutoStrategy {
+	return AutoStrategy{
+		basic: basic,
+		jwt:   jwt,
+	}
+}
+
+func (a AutoStrategy) AuthFunc() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		operator := middleware.AuthOperator{}
+		authHeader := strings.SplitN(c.Request.Header.Get("Authorization"), " ", 2)
+
+		if len(authHeader) != authHeaderCount {
+			core.WriteResponse(
+				c,
+				errors.WithCode(code.ErrInvalidAuthHeader, "Authorization header format is wrong"),
+				nil,
+			)
+			c.Abort()
+
+			return
+		}
+
+		switch authHeader[0] {
+		case "Basic":
+			operator.SetStrategy(a.basic)
+		case "Bearer":
+			operator.SetStrategy(a.jwt)
+		default:
+			core.WriteResponse(
+				c,
+				errors.WithCode(code.ErrSignatureInvalid, "unrecognized Authorization header"),
+				nil,
+			)
+			c.Abort()
+
+			return
+		}
+
+		operator.AuthFunc()(c)
+
+		c.Next()
+	}
+}
